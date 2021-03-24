797f955a74f6409c69b3b04d8585705d4d6be249
koltsun-nIkitos <60855708+koltsun-nIkitos@users.noreply.github.com>
2021-03-24 14:59:01 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@ var swiper = new Swiper('.swiper-container', {
     clickable: true
   },
 });
+
+
+
+/* Функция плавной прокрутки */
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
